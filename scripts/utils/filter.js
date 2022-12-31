f4e8dc097572e1ae88252cceda363f4665c24659
Damien BELGHIT <damien.belghit@yahoo.fr>
2022-12-31 12:21:03 +0100

    Les listes de filtres s'agrandissent sur focus

--- scripts/utils/filter.js
+++ scripts/utils/filter.js
@@ -1,12 +1,25 @@
 //Get DOM elements
 const filters = document.querySelectorAll(".filter");
 
+//Set first-placeholder parameter to initial placeholder
+filters.forEach((filter)=>{
+    const filterInput = filter.querySelector("input");
+    filter.setAttribute("first-placeholder", filterInput.placeholder);
+})
+
+//Increase/Decrease filter list size and changes placeholder
+function setFilterWide(filter, widen){
+    const filterInput = filter.querySelector("input");
+    filterInput.placeholder = widen ? filter.getAttribute("second-placeholder") : filter.getAttribute("first-placeholder");
+    filter.setAttribute("widen", widen);
+}
+
 //Closes all filter lists when there's a click outside them
 window.addEventListener("click", (e)=>{
     !(Array.from(filters).some((filter) => filter.contains(e.target))) && closeAllFilterLists()
 });
 
-//Expands filter list on click
+//Events expanding filter list on click
 filters.forEach((filter)=>{
     filter.addEventListener("click", ()=>{
         closeAllFilterLists();
@@ -20,19 +33,23 @@ function closeAllFilterLists(){
     filters.forEach((filter)=>displayFilterList(filter, false));
 }
 
-//Change the display style of the filter list to block or none depending on boolean parameter
+//Change the display style of the filter list to block or none
 function displayFilterList(filter, display){
     const arrow_icon = filter.querySelector("i");
     const filter_list = filter.querySelector("ul");
+    const filterInput = filter.querySelector("input");
+
     if(display){
         arrow_icon.classList.remove("fa-angle-down");
         arrow_icon.classList.add("fa-angle-up");
         filter_list.style.display = "block";
         filter.querySelector("input").focus();
+        (filterInput.value.length < 2) && setFilterWide(filter, true);
     }else{
         arrow_icon.classList.remove("fa-angle-up");
         arrow_icon.classList.add("fa-angle-down");
         filter_list.style.display = "none";
+        setFilterWide(filter, false);
     }
 }
 
@@ -44,6 +61,7 @@ filters.forEach((filter) => {
         const filterElems = filter.querySelectorAll("ul li");
         filterElems.forEach((filterElem)=>{
         filterElem.style.display = filterElem.textContent.toLowerCase().includes(filterInput.value.toLowerCase()) ? "block" : "none";
+        setFilterWide(filter, (filterInput.value.length < 2) );
     })
     });
 });
